4823a38023550f379663ef503b5ad0c17f2b3434
Xander Miller <xander.miller@gmail.com>
2017-03-23 14:37:01 -0400

    refactors and improves flash message implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,15 @@
 module ApplicationHelper
 
+  def flash_class(name)
+    if name == 'notice'
+      return 'success'
+    elsif name == 'alert'
+      return 'danger'
+    elsif ['success', 'info', 'warning', 'danger'].include?(name)
+      return name
+    else
+      return 'info'
+    end
+  end
+
 end
