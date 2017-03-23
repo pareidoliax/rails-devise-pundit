a25e90785d2c182bbd15da9ecba3d15b72ab2959
Xander Miller <xander.miller@gmail.com>
2017-03-23 14:46:43 -0400

    finishes basic label controllers

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -6,24 +6,30 @@ class LabelsController < ApplicationController
   def create
     @label = Label.new(secure_params)
     unless @label.save
-      flash
+      flash[:danger] = 'Something went wrong, no new label created.'
     end
     redirect_back(fallback_location: labels_path)
   end
 
   def update
     @label = Label.find(params[:id])
+    unless @label.update_attributes(secure_params)
+      flash[:danger] = 'Something went wrong, failed to update label.'
+    end
     redirect_back(fallback_location: labels_path)
   end
 
   def destroy
     @label = Label.find(params[:id])
+    unless @label.destroy
+      flash[:danger] = 'Something went wrong, label not deleted.'
+    end
     redirect_back(fallback_location: labels_path)
   end
 
 private #--------------------------------------------------------
 
   def secure_params
-    params.require(:label).permit(:users, :name, :colour)
+    params.require(:label).permit(:name, :colour)
   end
 end
