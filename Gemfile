065f61e24e27caad131c81f947266cde8d57f136
Xander Miller <xander.miller@gmail.com>
2017-03-23 18:18:45 -0400

    installs rails 12 factor gem for heroku

--- Gemfile
+++ Gemfile
@@ -40,3 +40,6 @@ group :test do
   gem 'launchy'
   gem 'selenium-webdriver'
 end
+group :production do
+  gem 'rails_12factor'
+end
--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,6 @@ Bundler.require(*Rails.groups)
 
 module RailsDevisePundit
   class Application < Rails::Application
-    config.serve_static_assets = true
 
     config.generators do |g|
       g.test_framework :rspec,
