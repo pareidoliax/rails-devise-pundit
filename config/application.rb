d26e8d5110d89540c09da5d8eb0330a635d01074
Xander Miller <xander.miller@gmail.com>
2017-03-23 18:16:22 -0400

    changes asset pipeline configuration for heroku

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module RailsDevisePundit
   class Application < Rails::Application
+    config.serve_static_assets = true
 
     config.generators do |g|
       g.test_framework :rspec,
